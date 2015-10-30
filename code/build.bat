437e190345629cc5219d7f63214a29c6f494a492
Abenohn <jabel88keys@gmail.com>
2015-10-30 17:06:51 -0400

    update

--- code/build.bat
+++ code/build.bat
@@ -2,5 +2,5 @@
 
 mkdir ..\..\build\handmade
 pushd ..\..\build\handmade
-cl -FC -Zi ..\..\handmade\code\win32_handmade.cpp /link user32.lib Gdi32.lib
+cl -FC -Zi ..\..\handmade\code\win32_handmade.cpp /link user32.lib Gdi32.lib 
 popd
